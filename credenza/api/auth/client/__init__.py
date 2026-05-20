39c171c4f7d6ad5e634af1493fa2dc679812e44b
mikedarcy <mikedarcy@users.noreply.github.com>
2026-05-20 13:25:40 -0700

    Add license headers to non-empty __init__.py files.

--- credenza/api/auth/client/__init__.py
+++ credenza/api/auth/client/__init__.py
@@ -1,3 +1,18 @@
+# Copyright 2026 University of Southern California
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 DEFAULT_CLIENT_AUTH_URN = "urn:credenza:client:auth"
 DEFAULT_CLIENT_AUTH_MAX_SESSION_TTL = 1800
 DEFAULT_CLIENT_AUTH_MAX_ABSOLUTE_LIFETIME = 86400
